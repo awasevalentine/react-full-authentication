c4a6364797e71fd8064ecdca7cdbee140b9f838c
awasevalentine <54159265+awasevalentine@users.noreply.github.com>
2022-06-08 04:35:16 +0100

    Modified the logic for the get started button, such that it redirects to registeration page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,17 @@
 import {BsArrowRight } from 'react-icons/bs'
 import {Box, Button, SlideFade } from '@chakra-ui/react'
 import './css/home.css'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+    const {user} = useSelector((state)=> state.auth)
+
+    useEffect(()=>{
+
+    }, [user])
 
 
     return (
@@ -16,9 +25,18 @@ const Home = () => {
                     handling different authentication process.</p>
                 </section>
                 <section className='btn-box'>
-                <Button className='home-btn' rightIcon={<BsArrowRight />} colorScheme='#FF8C8C' variant='solid'>
-                    Get started
-                </Button>
+                    {
+                        user ? (
+                            <Button color="#242F9B" w="200px" className='home-btn' rightIcon={<BsArrowRight />} colorScheme='#FF8C8C' variant='solid'>
+                                <Link to='/dashboard'>Go to Dashboard</Link>
+                            </Button>
+                        ): (
+                            <Button className='home-btn' rightIcon={<BsArrowRight />} colorScheme='#FF8C8C' variant='solid'>
+                                <Link to='/register'>Get started</Link>
+                            </Button>
+                        )
+                    }
+
                 </section>
                 </Box>
             </section>
